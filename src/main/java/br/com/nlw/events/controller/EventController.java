25e9b06baea533ceceb5d2f0f7e1cc3c30b5aa9c
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-18 19:08:00 -0300

    added Event controller

---
+++ src/main/java/br/com/nlw/events/controller/EventController.java
@@ -0,0 +1,36 @@
+package br.com.nlw.events.controller;
+
+import br.com.nlw.events.model.Event;
+import br.com.nlw.events.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @PostMapping
+    public Event addNewEvent(@RequestBody Event newEvent) {
+        return service.addNewEvent(newEvent);
+    }
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return service.getAllEvents();
+    }
+
+    @GetMapping("/{prettyName}")
+    public ResponseEntity<Event> getEventByPrettyName(@PathVariable String prettyName) {
+        Event event = service.getByPrettyName(prettyName);
+        if (event != null) {
+            return ResponseEntity.ok().body(event);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
