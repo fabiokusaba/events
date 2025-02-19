8f91711a4a1eeb3943af21671f148c33d438b7d1
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-18 23:02:30 -0300

    created custom exception to user indicator

---
+++ src/main/java/br/com/nlw/events/exception/UserIndicadorNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.nlw.events.exception;
+
+public class UserIndicadorNotFoundException extends RuntimeException {
+    public UserIndicadorNotFoundException(String message) {
+        super(message);
+    }
+}
