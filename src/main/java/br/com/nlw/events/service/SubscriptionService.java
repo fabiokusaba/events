37f7998022341cdd77e59b4c2b543470d18b7aa9
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-19 18:58:17 -0300

    implemented ranking by user

--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,5 +1,6 @@
 package br.com.nlw.events.service;
 
+import br.com.nlw.events.dto.SubscriptionRankingByUser;
 import br.com.nlw.events.dto.SubscriptionRankingItem;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Service
 public class SubscriptionService {
@@ -86,5 +88,25 @@ public class SubscriptionService {
             throw new EventNotFoundException("Ranking do evento " + prettyName + " não existe.");
         }
         return subscriptionRepository.generateRanking(event.getEventId());
+    }
+
+    public SubscriptionRankingByUser getRankingByUser(String prettyName, Integer userId) {
+        List<SubscriptionRankingItem> ranking = getCompleteRanking(prettyName);
+
+        SubscriptionRankingItem item = ranking.stream()
+                .filter(i -> i.userId().equals(userId))
+                .findFirst()
+                .orElse(null);
+
+        if (item == null) {
+            throw new UserIndicadorNotFoundException("Não há inscrições com indicação do usuário " + userId);
+        }
+
+        Integer posicao = IntStream.range(0, ranking.size())
+                .filter(pos -> ranking.get(pos).userId().equals(userId))
+                .findFirst()
+                .getAsInt();
+
+        return new SubscriptionRankingByUser(item, posicao + 1);
     }
 }
